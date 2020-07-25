572ada30f1756b70eed12637545261c029232793
Vlad <vladgotodo@gmail.com>
2020-07-25 15:06:15 -0400

    added class for response entities serialization

---
+++ src/main/java/Post.java
@@ -0,0 +1,31 @@
+import java.io.Serializable;
+
+public class Post implements Serializable {
+    private final int userId;
+    private final int id;
+    private final String title;
+    private final String body;
+
+    public Post(int userId, int id, String title, String body) {
+        this.userId = userId;
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+}
