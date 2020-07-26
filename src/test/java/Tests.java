9080f06f9d413424747e11954e3a503dff07ee79
Vlad <vladgotodo@gmail.com>
2020-07-25 22:39:18 -0400

    added schema validation

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
@@ -17,13 +18,21 @@ public class Tests extends BaseTest {
                 .get("?userId={id}", 5)
                 .then()
                 .statusCode(200)
-                .body("id[1]", equalTo(42)).log().ifError();
+                .body("id[1]", equalTo(42))
+                .assertThat()
+                .body(matchesJsonSchemaInClasspath("schema_posts.json"))
+                .log().ifError();
     }
 
     @Test
     public void checkGetListOfAllResourcesSimple() {
         Response response = when()
                 .get();
+
+        when()
+                .get()
+                .then()
+                .assertThat().body(matchesJsonSchemaInClasspath("schema_posts.json"));
 
         List<Post> returnedArtworks = Arrays.asList(response.getBody().as(Post[].class));
 
@@ -34,6 +43,11 @@ public class Tests extends BaseTest {
     public void checkFilterByQueryParametersSimpleWithRandomUserId() {
         //assuming that every user must have at least one post
         int randomUserId = (int)(Math.random()*((10-1)+1))+1;
+
+        when()
+                .get("?userId={id}", randomUserId)
+                .then()
+                .assertThat().body(matchesJsonSchemaInClasspath("schema_posts.json"));
 
         Response response = when()
                 .get("?userId={id}", randomUserId);
@@ -47,6 +61,11 @@ public class Tests extends BaseTest {
     public void checkGetResourceByRandomIdSimple() {
         //assuming that every post's id is unique
         int randomPostId = (int)(Math.random()*((100-1)+1))+1;
+
+        when()
+                .get("?id={id}", randomPostId)
+                .then()
+                .assertThat().body(matchesJsonSchemaInClasspath("schema_posts.json"));
 
         Response response = when()
                         .get("?id={id}", randomPostId);
